122d26ef0bf1051269362e4096368b8a9258cbd2
Valentin Funk <funk.valentin@gmail.com>
2016-07-04 19:17:41 +0200

    Add Products Frontend

--- src/app/main/products.controller.js
+++ src/app/main/products.controller.js
@@ -2,10 +2,22 @@
 
 angular.module('ms-web')
 
-.controller('ProductsCtrl', function($scope, $loading, ProductsService, UsersService) {  
-  ProductsService.list().then(function(products) {
-    $scope.products = products;
-  }).catch(function(error) {
-    $scope.error = error;
-  });
+.controller('ProductsCtrl', function($scope, $loading, ProductsService, UsersService) {
+  var loadList = function( ) {
+    ProductsService.list().then(function(products) {
+      console.log(products);
+      $scope.products = products;
+    }).catch(function(error) {
+      $scope.error = error;
+    });
+  }
+  loadList();
+
+  $scope.addProduct = function(product) {
+    $loading.start("product")
+    ProductsService.create(product).then(loadList).then(function() {
+      $loading.finish("product");
+      $scope.product = {};
+    });
+  }
 });
--- src/app/services/ProductsService.js
+++ src/app/services/ProductsService.js
@@ -7,12 +7,27 @@ angular.module('ms-web')
   ProductsService.list = function() {
     return $http({
       method: 'GET',
-      url: ServiceConfig.productsServiceUrl + '/',
+      url: ServiceConfig.productsServiceUrl + '/products',
       headers: {
         Authorization: "JWT " + UsersService.accessToken
       }
+    }).then(function(response){
+      return response.data;
     });
   };
+
+  ProductsService.create = function(product) {
+    return $http({
+      method: 'POST',
+      url: ServiceConfig.productsServiceUrl + '/products',
+      headers: {
+        Authorization: "JWT " + UsersService.accessToken
+      },
+      data: product
+    }).then(function(response){
+      return response.data;
+    });
+  }
 
   return ProductsService;
 });
